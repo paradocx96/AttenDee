4820e7aee812750a41ace8f90515402a7b822135
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-08-13 13:42:38 +0530

    Welcome Screen(main Activity) updated

--- app/src/main/java/com/androx/attendee/MainActivity.java
+++ app/src/main/java/com/androx/attendee/MainActivity.java
@@ -2,13 +2,26 @@ package com.androx.attendee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMEOUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intentHome = new Intent(MainActivity.this, HomepageActivity.class);
+                startActivity(intentHome);
+                finish();
+            }
+        },SPLASH_TIMEOUT);
     }
 }
