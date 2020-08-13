2afb0c579b54164d2956164795756fb7b8da5c97
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-08-13 12:49:22 +0530

    Initial commit 2020-08-13

---
+++ app/src/main/java/com/androx/attendee/EditEventActivity.java
@@ -0,0 +1,14 @@
+package com.androx.attendee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_event);
+    }
+}
---
+++ app/src/main/java/com/androx/attendee/EventHomeActivity.java
@@ -0,0 +1,14 @@
+package com.androx.attendee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_home);
+    }
+}
