ee7e347c14608fce06d1352d204452cd54eac12d
Navinda Lankesh <47824016+paradocx96@users.noreply.github.com>
2020-08-17 11:01:14 +0530

    Final 2020-08-17

--- app/src/main/java/com/androx/attendee/EditEventActivity.java
+++ app/src/main/java/com/androx/attendee/EditEventActivity.java
@@ -34,13 +34,6 @@ public class EditEventActivity extends AppCompatActivity {
             }
         });
 
-        editEventButton =(Button) findViewById(R.id.EditEvent);
-        editEventButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openActivityEditEvent();
-            }
-        });
     }
 
     public void openActivityEvents(){
@@ -53,8 +46,4 @@ public class EditEventActivity extends AppCompatActivity {
         startActivity(intent5);
     }
 
-    public void openActivityEditEvent(){
-        Intent intent6 = new Intent(this, EditEventActivity.class);
-        startActivity(intent6);
-    }
 }
