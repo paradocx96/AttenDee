bf8e3a99d89cb58615840ddad836efc202ccb2c6
declanChirush <hirushgimhan2016@gmail.com>
2020-10-03 16:17:07 +0530

    UI update done !!!

--- app/src/main/java/com/androx/attendee/AddAttendeeActivity.java
+++ app/src/main/java/com/androx/attendee/AddAttendeeActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -28,37 +27,37 @@ public class AddAttendeeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_attendee);
 
-        button1 = (Button) findViewById(R.id.btn1_menu);
+        // button1 = (Button) findViewById(R.id.btn1_menu);
         button2 = (Button) findViewById(R.id.btn2_menu);
-        button3 = (Button) findViewById(R.id.btn3_menu);
-        button4 = (Button) findViewById(R.id.btn4_menu);
-        button5 = (Button) findViewById(R.id.btn5_menu);
+        // button3 = (Button) findViewById(R.id.btn3_menu);
+        // button4 = (Button) findViewById(R.id.btn4_menu);
+        // button5 = (Button) findViewById(R.id.btn5_menu);
         addAttendBtn = (Button) findViewById(R.id.addPersonBtn);
-        name = findViewById(R.id.editTxt_personName);
-        remarks = findViewById(R.id.editTxt_remarks);
+        name = findViewById(R.id.update_editTxt_personName);
+        remarks = findViewById(R.id.update_editTxt_remarks);
 
         context = this;
         myDatabaseHelper = new MyDatabaseHelper(context);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                startActivity(new Intent(AddAttendeeActivity.this,AddEventActivity.class));
-//                button1.setBackgroundColor(Color.parseColor("#ffffff"));
-//                button1.setTextColor(Color.parseColor("#9c27b0"));
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                startActivity(new Intent(AddAttendeeActivity.this,AddEventActivity.class));
+////                button1.setBackgroundColor(Color.parseColor("#ffffff"));
+////                button1.setTextColor(Color.parseColor("#9c27b0"));
+////
+////
+////                button2.setBackgroundColor(Color.parseColor("#9c27b0"));
+////                button2.setTextColor(Color.parseColor("#ffffff"));
+////
+////
+////                button3.setBackgroundColor(Color.parseColor("#9c27b0"));
+////                button3.setTextColor(Color.parseColor("#ffffff"));
 //
 //
-//                button2.setBackgroundColor(Color.parseColor("#9c27b0"));
-//                button2.setTextColor(Color.parseColor("#ffffff"));
-//
-//
-//                button3.setBackgroundColor(Color.parseColor("#9c27b0"));
-//                button3.setTextColor(Color.parseColor("#ffffff"));
-
-
-            }
-        });
+//            }
+//        });
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,59 +80,59 @@ public class AddAttendeeActivity extends AppCompatActivity {
             }
         });
 
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-//                button3.setBackgroundColor(Color.parseColor("#ffffff"));
-//                button3.setTextColor(Color.parseColor("#9c27b0"));
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
 //
-//                button1.setBackgroundColor(Color.parseColor("#9c27b0"));
-//                button1.setTextColor(Color.parseColor("#ffffff"));
 //
-//                button2.setBackgroundColor(Color.parseColor("#9c27b0"));
-//                button2.setTextColor(Color.parseColor("#ffffff"));
-
-
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-                Intent intent = new Intent(AddAttendeeActivity.this, AddAttendeeActivity.class);
-                startActivity(intent);
-
-
-//                button4.setBackgroundColor(Color.parseColor("#ff9100"));
-//                button4.setTextColor(Color.parseColor("#ffffff"));
+////                button3.setBackgroundColor(Color.parseColor("#ffffff"));
+////                button3.setTextColor(Color.parseColor("#9c27b0"));
+////
+////                button1.setBackgroundColor(Color.parseColor("#9c27b0"));
+////                button1.setTextColor(Color.parseColor("#ffffff"));
+////
+////                button2.setBackgroundColor(Color.parseColor("#9c27b0"));
+////                button2.setTextColor(Color.parseColor("#ffffff"));
 //
-//                button5.setBackgroundColor(Color.parseColor("#ffffff"));
-//                button5.setTextColor(Color.parseColor("#ff9100"));
-
-            }
-        });
-
-
-        button5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(AddAttendeeActivity.this, AttendanceHomeActivity.class);
-                startActivity(intent);
-
-//                button5.setBackgroundColor(Color.parseColor("#ff9100"));
-//                button5.setTextColor(Color.parseColor("#ffffff"));
 //
-//                button4.setBackgroundColor(Color.parseColor("#ffffff"));
-//                button4.setTextColor(Color.parseColor("#ff9100"));
+//            }
+//        });
+
+//        button4.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//                Intent intent = new Intent(AddAttendeeActivity.this, AddAttendeeActivity.class);
+//                startActivity(intent);
+//
+//
+////                button4.setBackgroundColor(Color.parseColor("#ff9100"));
+////                button4.setTextColor(Color.parseColor("#ffffff"));
+////
+////                button5.setBackgroundColor(Color.parseColor("#ffffff"));
+////                button5.setTextColor(Color.parseColor("#ff9100"));
+//
+//            }
+//        });
 
 
-            }
-        });
+//        button5.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent intent = new Intent(AddAttendeeActivity.this, AttendanceHomeActivity.class);
+//                startActivity(intent);
+//
+////                button5.setBackgroundColor(Color.parseColor("#ff9100"));
+////                button5.setTextColor(Color.parseColor("#ffffff"));
+////
+////                button4.setBackgroundColor(Color.parseColor("#ffffff"));
+////                button4.setTextColor(Color.parseColor("#ff9100"));
+//
+//
+//            }
+//        });
 
         addAttendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
