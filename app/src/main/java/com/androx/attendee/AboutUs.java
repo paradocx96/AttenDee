a2c1cd80b1be035215cebe8ed05212af3f316025
vinuri-g <vinuri.galagoda@gmail.com>
2020-10-03 22:28:53 +0530

    AboutUs UI Added

---
+++ app/src/main/java/com/androx/attendee/AboutUs.java
@@ -0,0 +1,27 @@
+package com.androx.attendee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        Button btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUs.this, HomepageActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
